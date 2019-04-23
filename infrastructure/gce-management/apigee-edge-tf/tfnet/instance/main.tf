40812d4455d3842718e6762f25709e3c375df39f
Carlos Frias <friasc@google.com>
2019-04-23 14:02:29 -0400

    updated name

--- infrastructure/gce-management/apigee-edge-tf/tfnet/instance/main.tf
+++ infrastructure/gce-management/apigee-edge-tf/tfnet/instance/main.tf
@@ -7,7 +7,9 @@ variable "instance_type" {
   default = "n1-standard-1"
 }
 variable "instance_subnetwork" {}
-variable "instance_tags" {}
+variable "instance_tags" {
+  default =  []
+}
 
 resource "google_compute_instance" "vm_instance" {
   name         = "${var.instance_name}"
