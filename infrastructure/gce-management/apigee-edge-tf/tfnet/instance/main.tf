69ff0c0736d5afd191f49010e19b8d8f5ac919ba
Carlos Frias <friasc@google.com>
2019-04-23 14:03:16 -0400

    updated name

--- infrastructure/gce-management/apigee-edge-tf/tfnet/instance/main.tf
+++ infrastructure/gce-management/apigee-edge-tf/tfnet/instance/main.tf
@@ -24,7 +24,7 @@ resource "google_compute_instance" "vm_instance" {
   }
 
   network_interface {
-    subnetwork = "${var.instance_subnetwork}"
+    network = "${var.instance_subnetwork}"
 
     access_config {
       # Allocate a one-to-one NAT IP to the instance
