77d0d37bcc25bef936f791a2ed9d285ab9aadcaa
Carlos Frias <friasc@google.com>
2019-04-23 21:37:38 -0400

    updated name

--- infrastructure/gce-management/apigee-edge-tf/main.tf
+++ infrastructure/gce-management/apigee-edge-tf/main.tf
@@ -63,7 +63,7 @@ module "apigee-vm-0" {
 
 # Add an apigee-vm instance
 module "apigee-vm-1" {
-  source              = "./internal-instance"
+  source              = "./external-instance"
   instance_name       = "planet-edge-dc-1-ms-dc-1-ldap-dc-1-ds-1"
   instance_zone       = "us-central1-a"
   instance_network = "${google_compute_network.apigeenet.self_link}"
@@ -76,7 +76,7 @@ module "apigee-vm-1" {
 
 # Add an apigee-vm instance
 module "apigee-vm-2" {
-  source              = "./internal-instance"
+  source              = "./external-instance"
   instance_name       = "planet-edge-dc-1-ds-dc-1-rmp-1"
   instance_zone       = "us-central1-a"
   instance_network = "${google_compute_network.apigeenet.self_link}"
@@ -88,7 +88,7 @@ module "apigee-vm-2" {
 
 # Add an apigee-vm instance
 module "apigee-vm-3" {
-  source              = "./internal-instance"
+  source              = "./external-instance"
   instance_zone       = "us-central1-a"
   instance_name       = "planet-edge-dc-1-ds-dc-1-rmp-2"
   instance_network = "${google_compute_network.apigeenet.self_link}"
@@ -100,7 +100,7 @@ module "apigee-vm-3" {
 
 # Add an apigee-vm instance
 module "apigee-vm-4" {
-  source              = "./internal-instance"
+  source              = "./external-instance"
   instance_name       = "planet-edge-dc-1-pg-dc-1-pgmaster-dc-1-qpid-1"
   instance_zone       = "us-central1-a"
   instance_network = "${google_compute_network.apigeenet.self_link}"
@@ -112,7 +112,7 @@ module "apigee-vm-4" {
 
 # Add an apigee-vm instance
 module "apigee-vm-5" {
-  source              = "./internal-instance"
+  source              = "./external-instance"
   instance_name       = "planet-edge-dc-1-pg-dc-1-pgstandby-dc-1-qpid-2"
   instance_zone       = "us-central1-a"
   instance_network = "${google_compute_network.apigeenet.self_link}"
