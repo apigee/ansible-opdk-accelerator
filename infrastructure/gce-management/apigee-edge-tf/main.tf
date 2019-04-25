97804fcf9e3edbeb1f2a93f6aa97a1784b026217
Carlos Frias <friasc@google.com>
2019-04-24 22:52:01 -0400

    updated terraform

--- infrastructure/gce-management/apigee-edge-tf/main.tf
+++ infrastructure/gce-management/apigee-edge-tf/main.tf
@@ -113,7 +113,7 @@ resource "google_compute_disk" "apigeenet-ms" {
 
 data "google_compute_image" "apigeenet-ms" {
   family  = "centos-7"
-  project = "gce-uefi-images"
+  project = "centos-cloud"
 }
 
 # Add a firewall rule to allow HTTP, SSH, and RDP traffic on apigeenet
