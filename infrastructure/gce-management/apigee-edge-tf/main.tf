b9d59f0bcc7b342f0662ebd27006fd1866c5fe7c
Carlos Frias <friasc@google.com>
2019-04-25 11:08:20 -0400

    updated terraform

--- infrastructure/gce-management/apigee-edge-tf/main.tf
+++ infrastructure/gce-management/apigee-edge-tf/main.tf
@@ -116,7 +116,7 @@ resource "google_compute_disk" "apigeenet-ms" {
 }
 
 data "google_compute_image" "apigeenet-ms" {
-  family  = "centos-7-v20190423"
+  image_id  = "centos-7-v20190423"
   project = "centos-cloud"
 }
 
