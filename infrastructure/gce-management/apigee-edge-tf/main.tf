0e5027c853f105f58b7d9a7650e274869962a704
Carlos Frias <friasc@google.com>
2019-04-26 10:56:30 -0400

    updated terraform

--- infrastructure/gce-management/apigee-edge-tf/main.tf
+++ infrastructure/gce-management/apigee-edge-tf/main.tf
@@ -42,7 +42,7 @@ resource "google_compute_global_forwarding_rule" "apigeenet-ms" {
   name       = "apigeenet-ms"
   port_range = "80"
   ip_address = "${google_compute_global_address.apigeenet-ms.address}"
-  target     = "${module.apigee-vm-1.network_ip}"
+  target     = "${module.apigee-vm-1}"
 }
 
 //resource "google_compute_target_http_proxy" "apigeenet-ms" {
