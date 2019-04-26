38636fb0ba7ccabbd5858151899b139fa2d7c987
Carlos Frias <friasc@google.com>
2019-04-26 10:16:38 -0400

    updated terraform

--- infrastructure/gce-management/apigee-edge-tf/internal-instance/main.tf
+++ infrastructure/gce-management/apigee-edge-tf/internal-instance/main.tf
@@ -1,20 +1,15 @@
 variable "instance_name" {}
 variable "instance_zone" {}
-
 variable "instance_image" {
   default = "centos-7"
 }
-
 variable "instance_type" {
   default = "n1-standard-1"
 }
-
 variable "instance_network" {}
-
 variable "instance_tags" {
   default = []
 }
-
 variable "instance_external_ip" {
   default = ""
 }
@@ -24,18 +19,19 @@ resource "google_compute_instance" "vm_instance" {
   zone         = "${var.instance_zone}"
   machine_type = "${var.instance_type}"
   tags         = "${var.instance_tags}"
-
   boot_disk {
     initialize_params {
       image = "${var.instance_image}"
     }
   }
-
   network_interface {
     network = "${var.instance_network}"
+    access_config = {}
+
   }
 }
 
 output "private_address" {
   value = "${google_compute_instance.vm_instance.network_interface.network_ip}"
 }
+
