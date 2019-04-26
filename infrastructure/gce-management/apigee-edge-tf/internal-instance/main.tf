5991e2cfe6a9fe6cc0be44726cc6d449c3a7d3cc
Carlos Frias <friasc@google.com>
2019-04-26 10:04:40 -0400

    updated terraform

--- infrastructure/gce-management/apigee-edge-tf/internal-instance/main.tf
+++ infrastructure/gce-management/apigee-edge-tf/internal-instance/main.tf
@@ -37,5 +37,5 @@ resource "google_compute_instance" "vm_instance" {
 }
 
 output "private_address" {
-  value = "${google_compute_instance.vm_instance.network_interface[0].address}"
+  value = "${google_compute_instance.vm_instance.network_interface.network_ip}"
 }
