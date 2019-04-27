449da3854371305cca73a51656521323809faa02
Carlos Frias <friasc@google.com>
2019-04-26 22:11:02 -0400

    updated terraform

--- infrastructure/gce-management/apigee-edge-tf/external-instance/main.tf
+++ infrastructure/gce-management/apigee-edge-tf/external-instance/main.tf
@@ -50,6 +50,6 @@ resource "google_compute_instance" "vm_instance" {
   }
 }
 
-output "network_interface" {
+output "network_ip" {
   value = "${google_compute_instance.vm_instance.network_interface.0.network_ip}"
 }
