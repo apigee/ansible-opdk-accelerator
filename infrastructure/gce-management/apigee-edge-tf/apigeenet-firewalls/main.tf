d5c53a03e4ec2fe2e46d48c22dd74a63c46d1a3b
Carlos Frias <friasc@google.com>
2019-05-05 23:12:28 -0400

    updated terraform format

--- infrastructure/gce-management/apigee-edge-tf/apigeenet-firewalls/main.tf
+++ infrastructure/gce-management/apigee-edge-tf/apigeenet-firewalls/main.tf
@@ -1,8 +1,10 @@
 variable "firewall_name" {}
 variable "firewall_network" {}
+
 variable "firewall_source_ranges" {
   type = "list"
 }
+
 variable "firewall_protocol" {}
 
 variable "firewall_ports" {
