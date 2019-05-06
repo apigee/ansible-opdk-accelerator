990b5a53a98a30a27d37957837337688231e42b1
Carlos Frias <friasc@google.com>
2019-05-05 22:55:47 -0400

    Added firewall module

--- infrastructure/gce-management/apigee-edge-tf/apigeenet-firewalls/main.tf
+++ infrastructure/gce-management/apigee-edge-tf/apigeenet-firewalls/main.tf
@@ -8,6 +8,19 @@ variable "firewall_ports" {
 
 # Add a firewall rule to allow HTTP, SSH, and RDP traffic on apigeenet
 resource "google_compute_firewall" "apigeenet-allow-tcp-icmp" {
+  count = "${length(var.firewall_ports)}"
+  name    = "${var.firewall_name}"
+  network = "${var.firewall_network}"
+
+  source_ranges = "${var.firewall_source_ranges}"
+
+  allow {
+    protocol = "${var.firewall_protocol}"
+  }
+}
+
+resource "google_compute_firewall" "apigeenet-allow-tcp-icmp" {
+  count = "%{ if length(var.firewall_ports) > 0 }1%{else}0%{endif}"
   name    = "${var.firewall_name}"
   network = "${var.firewall_network}"
 
