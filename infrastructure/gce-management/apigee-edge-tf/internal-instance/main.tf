e350195249405375277a71b671bdefbb40aa5760
Carlos Frias <friasc@google.com>
2019-05-06 16:51:32 -0400

    updated logic

--- infrastructure/gce-management/apigee-edge-tf/internal-instance/main.tf
+++ infrastructure/gce-management/apigee-edge-tf/internal-instance/main.tf
@@ -28,7 +28,6 @@ resource "google_compute_instance" "vm_instance" {
   boot_disk {
     initialize_params {
       image = "${var.instance_image}"
-
       //      size = 60
     }
   }
--- infrastructure/gce-management/apigee-edge-tf/provider.tf
+++ infrastructure/gce-management/apigee-edge-tf/provider.tf
@@ -1,1 +1,5 @@
-provider "google" {}
+provider "google" {
+  credentials = "${file("~/.apigee-secure/edge-ops-lab-e7a48eec6f40.json")}"
+  project = "edge-labs-ops"
+  region = "us-central1"
+}
