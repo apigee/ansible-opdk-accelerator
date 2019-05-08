57786ca804a24c47cca1308c045b46db1c57442e
Carlos Frias <friasc@google.com>
2019-05-08 17:46:42 -0400

    updated settings

--- infrastructure/gce-management/apigee-edge-tf/main.tf
+++ infrastructure/gce-management/apigee-edge-tf/main.tf
@@ -186,7 +186,6 @@ module "apigee-bastion-vm" {
 
   instance_external_ip  = "Ephemeral"
   instance_scopes       = ["compute-rw", "storage-ro"]
-//  service_account_email = "313223894095-compute@developer.gserviceaccount.com"
 }
 
 # Add an apigee-vm instance
