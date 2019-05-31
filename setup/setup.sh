e27c76257a50537db83966e87ab285f5a479e443
Carlos Frias <friasc@google.com>
2019-05-31 13:20:11 -0400

    updated config

--- setup/setup.sh
+++ setup/setup.sh
@@ -3,6 +3,6 @@
 sudo yum install -y python-pip
 sudo pip install pip --upgrade
 sudo pip install ansible google-auth
-ansible-galaxy import https://github.com/carlosfrias/apigee-opdk-setup-ansible-controller.git
-ansible-galaxy import https://github.com/carlosfrias/apigee-opdk-modules.git
+ansible-galaxy import carlosfrias https://github.com/carlosfrias/apigee-opdk-setup-ansible-controller.git
+ansible-galaxy import carlosfrias https://github.com/carlosfrias/apigee-opdk-modules.git
 
