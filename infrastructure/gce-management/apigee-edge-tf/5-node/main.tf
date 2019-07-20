ec699ab6cfbb3068e8c794a37a547af22da06046
Carlos Frias <friasc@google.com>
2019-07-19 21:23:03 -0400

    Created infrastructure for 5 node planet.

---
+++ infrastructure/gce-management/apigee-edge-tf/5-node/main.tf
@@ -0,0 +1,173 @@
+# Create the apigeenet network
+resource "google_compute_network" "apigeenet" {
+  name                    = "apigeenet"
+  auto_create_subnetworks = true
+}
+
+# Create the apigeenet router
+resource "google_compute_router" "apigeenet-router" {
+  name    = "apigeenet-router"
+  network = "${google_compute_network.apigeenet.self_link}"
+}
+
+# Create the apigeenet router
+resource "google_compute_router" "apigeenet-central-router" {
+  name    = "apigeenet-router"
+  network = "${google_compute_network.apigeenet.self_link}"
+  region  = "us-central1"
+}
+
+# Create the gateway nat for the apigeenet-subnet-router
+resource "google_compute_router_nat" "apigeenet-subnet-nat" {
+  name                               = "apigeenet-subnet-nat"
+  router                             = "${google_compute_router.apigeenet-router.name}"
+  region                             = "${var.region}"
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
+# Create the gateway nat for the apigeenet-subnet-router
+resource "google_compute_router_nat" "apigeenet-subnet-nat-dc-2" {
+  name = "apigeenet-subnet-nat-dc-2"
+  //  router                             = "us-east1/apigeenet"
+  router                             = "${google_compute_router.apigeenet-central-router.name}"
+  region                             = "us-central1"
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
+
+module "configure_firewall_apigeenet_allow_mgmt_ui" {
+  source                 = "../modules/apigeenet-firewalls-protocol-with-ports"
+  firewall_name          = "mgmt-ui"
+  firewall_source_tags   = ["mgmt-ui"]
+  firewall_network       = "${google_compute_network.apigeenet.self_link}"
+  firewall_protocol      = "tcp"
+  firewall_ports         = ["9000", "80", "8080", "9001"]
+  firewall_source_ranges = ["10.0.0.0/8"]
+}
+
+module "configure_firewall_apigeenet_allow_icmp" {
+  source                 = "../modules/apigeenet-firewalls-protocol-only"
+  firewall_name          = "apigeenet-allow-icmp"
+  firewall_network       = "${google_compute_network.apigeenet.self_link}"
+  firewall_protocol      = "icmp"
+  firewall_source_ranges = ["10.0.0.0/8"]
+}
+
+module "configure_firewall_apigeenet_allow_ssh" {
+  source                 = "../modules/apigeenet-firewalls-protocol-with-ports"
+  firewall_name          = "apigeenet-allow-ssh"
+  firewall_source_tags   = ["apigeenet-allow-ssh"]
+  firewall_network       = "${google_compute_network.apigeenet.self_link}"
+  firewall_protocol      = "tcp"
+  firewall_ports         = ["22"]
+  firewall_source_ranges = ["10.0.0.0/8"]
+}
+
+module "configure_firewall_public_allow_ssh" {
+  source                 = "../modules/apigeenet-firewalls-protocol-with-ports"
+  firewall_name          = "public-allow-ssh"
+  firewall_source_tags   = ["public-allow-ssh"]
+  firewall_network       = "${google_compute_network.apigeenet.self_link}"
+  firewall_protocol      = "tcp"
+  firewall_ports         = ["22"]
+  firewall_source_ranges = ["0.0.0.0/0"]
+}
+
+module "configure_firewall_apigeenet_allow_local" {
+  source                 = "../modules/apigeenet-firewalls-protocol-with-ports"
+  firewall_name          = "apigeenet-allow-local"
+  firewall_source_tags   = ["apigeenet-allow-local"]
+  firewall_network       = "${google_compute_network.apigeenet.self_link}"
+  firewall_protocol      = "tcp"
+  firewall_ports         = ["0-65535"]
+  firewall_source_ranges = ["10.0.0.0/8"]
+}
+
+//# Add an apigee-vm instance
+module "apigee-bastion" {
+  source           = "../modules/external-instance"
+  instance_name    = "apigee-bastion"
+  instance_zone    = "${var.zone}"
+  instance_network = "${google_compute_network.apigeenet.self_link}"
+  instance_type    = "n1-standard-1"
+
+  instance_tags = [
+    "apigeenet-allow-icmp",
+    "public-allow-ssh",
+    "apigeenet-allow-local",
+    "g-on-g-notify-ignore",
+  ]
+
+  instance_external_ip = "Ephemeral"
+  instance_scopes      = ["compute-rw", "storage-ro"]
+}
+
+# Add an apigee-vm instance
+module "apigee-dc-1-ms-ldap-ui-ds" {
+  source             = "../modules/internal-instance"
+  instance_name      = "planet-dc-1-ms-dc-1-ldap-dc-1-ui-dc-1-ds"
+  instance_zone      = "${var.zone}"
+  instance_network   = "${google_compute_network.apigeenet.self_link}"
+  instance_disk_size = 250
+  instance_type      = "n1-standard-2"
+
+  instance_tags = [
+    "apigeenet-allow-ssh",
+    "apigeenet-allow-icmp",
+    "apigeenet-allow-mgmt-ui",
+    "apigeenet-allow-local",
+  ]
+}
+
+# Add an apigee-vm instance
+module "apigee-dc-1-ds-rmp" {
+  source             = "../modules/internal-instance"
+  instance_zone      = "${var.zone}"
+  instance_count     = "2"
+  instance_name      = "planet-dc-1-ds-dc-1-rmp"
+  instance_network   = "${google_compute_network.apigeenet.self_link}"
+  instance_disk_size = 250
+  instance_type      = "n1-standard-2"
+
+  instance_tags = [
+    "apigeenet-allow-ssh",
+    "apigeenet-allow-icmp",
+    "apigeenet-allow-mgmt-ui",
+    "apigeenet-allow-local",
+  ]
+}
+
+//# Add an apigee-vm instance
+module "apigee-dc-1-qpid-pg-pgmaster" {
+  source             = "../modules/internal-instance"
+  instance_zone      = "${var.zone}"
+  instance_name      = "planet-dc-1-qpid-dc-1-pg-dc-1-pgmaster"
+  instance_network   = "${google_compute_network.apigeenet.self_link}"
+  instance_disk_size = 250
+  instance_type      = "n1-standard-2"
+
+  instance_tags = [
+    "apigeenet-allow-ssh",
+    "apigeenet-allow-icmp",
+    "apigeenet-allow-mgmt-ui",
+    "apigeenet-allow-local",
+  ]
+}
+
+# Add an apigee-vm instance
+module "apigee-dc-1-qpid-pg-pgstandby" {
+  source             = "../modules/internal-instance"
+  instance_name      = "planet-dc-1-qpid-dc-1-pg-dc-1-pgstandby"
+  instance_zone      = "${var.zone}"
+  instance_network   = "${google_compute_network.apigeenet.self_link}"
+  instance_disk_size = 250
+  instance_type      = "n1-standard-2"
+
+  instance_tags = [
+    "apigeenet-allow-ssh",
+    "apigeenet-allow-icmp",
+    "apigeenet-allow-mgmt-ui",
+    "apigeenet-allow-local",
+  ]
+}
+
---
+++ infrastructure/gce-management/apigee-edge-tf/5-node/variable.tf
@@ -0,0 +1,75 @@
+variable "router_name" {
+  default = "default"
+}
+
+variable "region" {
+  default = "us-east1"
+}
+
+variable "zone" {
+  default = "us-east1-b"
+}
+
+variable "image_name" {
+  default = "centos-7"
+}
+
+variable "image_project" {
+  default = "centos-cloud"
+}
+
+variable "machine_type" {
+  default = "n1-standard-4"
+}
+
+variable "instance_scopes" {
+  default = ["compute-rw", "storage-ro"]
+}
+
+variable "service_account_email" {
+  default = "736255665193-compute@developer.gserviceaccount.com"
+}
+
+variable "dc_region" {
+  default = "1"
+}
+
+variable "dc_1_ms_count" {
+  default = 1
+}
+
+variable "dc_2_ms_count" {
+  default = 0
+}
+
+variable "dc_1_ds_count" {
+  default = 3
+}
+
+variable "dc_2_ds_count" {
+  default = 0
+}
+
+variable "dc_1_rmp_count" {
+  default = 2
+}
+
+variable "dc_2_rmp_count" {
+  default = 0
+}
+
+variable "dc_1_qpid_count" {
+  default = 2
+}
+
+variable "dc_2_qpid_count" {
+  default = 0
+}
+
+variable "dc_1_pgstandby_count" {
+  default = 1
+}
+
+variable "dc_2_pgstandby_count" {
+  default = 0
+}
