8c6304100f1751491506e0612cfdf425e85afeae
Carlos Frias <friasc@google.com>
2019-07-24 09:26:36 -0700

    updated provider

--- infrastructure/gce-management/apigee-edge-tf/5-node/provider.tf
+++ infrastructure/gce-management/apigee-edge-tf/5-node/provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = "${${file(var.credentials_file)}}"
+  credentials = "${file(var.credentials_file)}"
   project     = "${var.gcp_project_name}"
   region      = "${var.region}"
 }
